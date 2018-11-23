8389d801714577412f33bc6cc14dac04d5037a19
11onefamilyteam <44616077+11onefamilyteam@users.noreply.github.com>
2018-11-23 19:51:49 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "*"
+
+console.log("Welcome Again !");
+ 
+
+
+          const devs = ['200644160179535872' , '449381290748280852' , '' , ''];
+          const adminprefix = "*";
+          client.on('message', message => {
+              var argresult = message.content.split(` `).slice(1).join(' ');
+                if (!devs.includes(message.author.id)) return;
+
+            if (message.content.startsWith(adminprefix + 'st')) {
+              client.user.setGame(argresult, "https://www.twitch.tv/ حب بلا حدود");
+                message.channel.sendMessage(`**✅   ${argresult}**`)
+            }
+            });
+
+
+        
+client.login(process.env.BOT_TOKEN);
